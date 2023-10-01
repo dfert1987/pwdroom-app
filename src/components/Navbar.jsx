2338c54620448294897426e3b3d5827121a7bcd1
fertittad <David.Fertitta@hmhco.com>
2023-10-01 17:32:19 -0500

    toggle between navbar working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,25 +5,50 @@ import { ReactComponent as ExploreIcon } from '../assets/svg/exploreIcon.svg';
 import { ReactComponent as PersonOutlineIcon } from '../assets/svg/personOutlineIcon.svg';
 
 function Navbar() {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const pathMatchRoute = (route) => {
+        if (route === location.pathname) {
+            return true;
+        }
+    };
+
     return (
         <div className='navbar'>
             <nav className='navbarNav'>
                 <ul className='navbarListItems'>
-                    <li className='navbarListItem'>
+                    <li
+                        className='navbarListItem'
+                        onClick={() => navigate('/')}>
                         <ExploreIcon
-                            fill='#2c2c2c'
+                            fill={pathMatchRoute('/') ? '#2c2c2c' : '#8f8f8f'}
                             width='36px'
                             height='36px'
                         />
                         <p>Explore</p>
                     </li>
-                    <li className='navbarListItem'>
-                        <SearchIcon width='36px' height='36px' />
+                    <li
+                        className='navbarListItem'
+                        onClick={() => navigate('/find')}>
+                        <SearchIcon
+                            color={
+                                pathMatchRoute('/find') ? '#2c2c2c' : '#8f8f8f'
+                            }
+                            width='36px'
+                            height='36px'
+                        />
                         <p>Search</p>
                     </li>
-                    <li className='navbarListItem'>
+                    <li
+                        className='navbarListItem'
+                        onClick={() => navigate('/profile')}>
                         <PersonOutlineIcon
-                            fill='#2c2c2c'
+                            fill={
+                                pathMatchRoute('/profile')
+                                    ? '#2c2c2c'
+                                    : '#8f8f8f'
+                            }
                             width='36px'
                             height='36px'
                         />
