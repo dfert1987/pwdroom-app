4c2aa5321652c3bd9acba01e6ac0d6c738e129bf
fertittad <David.Fertitta@hmhco.com>
2023-10-01 16:49:21 -0500

    some basic routes set up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Explore from './pages/explore';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import ForgotPassword from './pages/ForgotPassword';
+
 function App() {
     return (
         <>
-            <h1>My App</h1>
+            <Router>
+                <Routes>
+                    <Route path='/' element={<Explore />} />
+                    <Route path='/profile' element={<SignIn />} />
+                    <Route path='/sign-in' element={<SignIn />} />
+                    <Route path='/sign-up' element={<SignUp />} />
+                    <Route
+                        path='/forgot-password'
+                        element={<ForgotPassword />}
+                    />
+                </Routes>
+            </Router>
         </>
     );
 }
