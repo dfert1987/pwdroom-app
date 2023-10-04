d9bc7e0d0698d0e47434d6c7c11842023c6bbaa3
fertittad <David.Fertitta@hmhco.com>
2023-10-03 22:11:07 -0500

    dropdown added. constants file added. add a bathroom form started

---
+++ src/assets/constants.js
@@ -0,0 +1,47 @@
+export const NeighborhoodOptions = [
+    'Barton Hills',
+    'Cherrywood',
+    'Downtown',
+    'Hyde Park',
+    'North Loop',
+    'North University',
+    'Old West Austin',
+    'South Lamar',
+    'Tarrytown',
+    'Zilker',
+    'Allande',
+    'Cedar Park',
+    'Georgetown',
+    'Leander',
+    'Round Rock',
+    'Wells Branch',
+    'Buda',
+    'Circle C',
+    'Kyle',
+    'South Congress',
+    'East Austin',
+    'Mueller',
+    'Dripping Springs',
+    'Westlake',
+];
+
+export const DefaultNeighborhoodOption = NeighborhoodOptions[0];
+
+export const BarOptions = [
+    'Dive',
+    'Cocktail Bar',
+    'Honky-Tonk',
+    'Night Club',
+    'Patio Bar',
+    'Pub',
+    'Brewery',
+    'Wine Bar',
+    'Sports Bar',
+    'LGBTQ+ Bar',
+    'Distillery',
+    'Music Venue',
+    'Comedy Club',
+    'Other',
+];
+
+export const DefaultBarOption = BarOptions[0];
--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,7 +1,15 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
+import Dropdown from 'react-dropdown';
 import Spinner from '../components/Spinner';
+import {
+    NeighborhoodOptions,
+    DefaultNeighborhoodOption,
+    BarOptions,
+    DefaultBarOption,
+} from '../assets/constants';
+import 'react-dropdown/style.css';
 
 function CreateListing() {
     const [loading, setLoading] = useState(false);
@@ -27,7 +35,26 @@ function CreateListing() {
         longitude: 0,
     });
 
-    console.log(formData);
+    const {
+        type,
+        name,
+        subtype,
+        address,
+        neighborhood,
+        tag,
+        description,
+        single,
+        gendered,
+        overall,
+        cleanliness,
+        flat,
+        safety,
+        privacy,
+        vibes,
+        imageUrls,
+        latitude,
+        longitude,
+    } = formData;
 
     const auth = getAuth();
     const navigate = useNavigate();
@@ -50,11 +77,88 @@ function CreateListing() {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [isMounted]);
 
+    const onSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    const onMutate = (e) => {};
+
     if (loading) {
         return <Spinner />;
     }
 
-    return <div>CreateListing</div>;
+    return (
+        <div className='profile'>
+            <header>
+                <p className='pageHeader'>Add a Bathroom</p>
+            </header>
+
+            <main>
+                <form onSubmit={onSubmit}>
+                    <label className='formLabel'>Bar / Restaurant</label>
+                    <div className='formButtons'>
+                        <button
+                            type='button'
+                            className={
+                                type === 'bar'
+                                    ? 'formButtonActive'
+                                    : 'formButton'
+                            }
+                            id='type'
+                            value='bar'
+                            onClick={onMutate}>
+                            Bar
+                        </button>
+                        <button
+                            type='button'
+                            className={
+                                type === 'restaurant'
+                                    ? 'formButtonActive'
+                                    : 'formButton'
+                            }
+                            id='type'
+                            value='restaurant'
+                            onClick={onMutate}>
+                            Restaurant
+                        </button>
+                    </div>
+                    <label className='formLabel'> Subtype: </label>
+                    <Dropdown
+                        className='neighborhoodDropdown'
+                        options={BarOptions}
+                        onChange={onMutate}
+                        value={subtype}
+                        placeholder='Choose a bar type'
+                        controlClassName='myControlClassName'
+                        arrowClassName='myArrowClassName'
+                        placeholderClassName='myPlaceholderClassName'
+                    />
+                    <label className='formLabel'>Name:</label>
+                    <input
+                        type='text'
+                        className='formInputName'
+                        id='name'
+                        value={name}
+                        onChange={onMutate}
+                        maxLength={32}
+                        minLength={5}
+                        required
+                    />
+                    <label className='formLabel'> Neighborhood: </label>
+                    <Dropdown
+                        className='neighborhoodDropdown'
+                        options={NeighborhoodOptions}
+                        onChange={onMutate}
+                        value={neighborhood}
+                        placeholder='Choose a Neighborhood'
+                        controlClassName='myControlClassName'
+                        arrowClassName='myArrowClassName'
+                        placeholderClassName='myPlaceholderClassName'
+                    />
+                </form>
+            </main>
+        </div>
+    );
 }
 
 export default CreateListing;
